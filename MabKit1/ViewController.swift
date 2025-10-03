20704b0ffae7d94943b4d174ec63fd31d26ae61f
Ahmad Alshafei <ahmadmarzoq44@gmail.com>
2025-10-03 21:08:57 +0300

    Initial Commit

---
+++ MabKit1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MabKit1
+//
+//  Created by AhmadALshafei on 10/3/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
